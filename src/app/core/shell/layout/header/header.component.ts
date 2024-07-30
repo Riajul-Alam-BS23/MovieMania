e7bc75edae1fda12840e6425b295569ce4c7d1d1
Md. Riajul Alam Bhuiyan <riajul.alam@brainstation-23.com>
2024-07-30 19:09:23 +0600

    design header

---
+++ src/app/core/shell/layout/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  @ViewChild('moviesMenuTrigger') moviesMenuTrigger: MatMenuTrigger;
+
+}
