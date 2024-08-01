9b0bcda5aa919ad272d1b8401e4e8759f403d969
Riajul Alam <riajulalam5@gmail.com>
2024-08-01 14:43:16 +0600

    update header

--- src/app/hover-menu.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HoverMenuDirective } from './hover-menu.directive';
-
-describe('HoverMenuDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HoverMenuDirective();
-    expect(directive).toBeTruthy();
-  });
-});
