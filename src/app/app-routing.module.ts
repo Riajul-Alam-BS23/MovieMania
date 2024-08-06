14f5d701540d8fe0a70fe4eda7f1916b7183b2d6
Md. Riajul Alam Bhuiyan <riajul.alam@brainstation-23.com>
2024-08-07 03:54:37 +0600

    add movie details design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieComponent } from './features/movie/movie.component';
 
 const routes: Routes = [
+  {
+    path:'movie',
+    component: MovieComponent
+  }
 ];
 
 @NgModule({
